11f9f33019ade59572bd3112e6b44c19e908ec1f
Adi Tanase <ady00t@live.com>
2015-05-11 10:31:13 +0200

    first init

---
+++ MyClass/src/myclass/MyClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclass;
+
+
+/**
+ *
+ * @author Braxis
+ */
+public class MyClass {
+ 
+    public double testMethodOne(int a, int b) {
+        return a/b;
+    }
+ 
+}
+
+    /**
+     * @param args the command line arguments
+     */
+ //   public static void main(String[] args) {
+        // TODO code application logic here
+ 
